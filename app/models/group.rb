2a51c0577626538871aed4562bc13175f3322d4c
Ben Jervis <ben@jervis.net.au>
2017-05-27 21:15:24 +1000

    Added groups model

---
+++ app/models/group.rb
@@ -0,0 +1,29 @@
+class Group < ApplicationRecord
+	has_many :memberships
+	has_many :users, through: :memberships
+
+	validates :name, presence: true
+
+	def member?(user)
+		self.users.include?(user.to_i)
+	end
+
+	def make_admin(user)
+		membership = self.memberships.select{ |membership| membership.user_id == user.to_i }.first
+		membership.update_attributes(trusted: true)
+		membership.save
+	end
+
+	def admins
+		self.memberships.select(&:trusted).collect(&:user_id)
+	end
+
+	def admin?(user)
+		admins.include?(user.to_i)
+	end
+
+	def to_i
+		self.id
+	end
+
+end
