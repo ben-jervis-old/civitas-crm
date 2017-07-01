4c2cdd47ccc2cdc17e0a09c254906f0a60e4044c
jrn <jrn889@uowmail.edu.au>
2017-07-01 09:05:16 +0000

    Added index page to show all users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,5 @@ class UsersController < ApplicationController
 	def new
 		@user = User.new
 	end
+	
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
 	root 'users#new'
+	
+	get  '/users',   to: 'users#index'
+
 end
