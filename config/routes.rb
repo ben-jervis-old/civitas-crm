060c158067aa69110cb8ff7ad81b7889cd197655
Ben Jervis <ben@jervis.net.au>
2017-07-02 18:39:47 +1000

    Dashboard template in place

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,8 @@ Rails.application.routes.draw do
   resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
-	root 'users#new'
-	
+	root 'dashboard#index'
+
 	get '/users', to: "users#index"
   get '/users/:id', to: "users#show"
 end
