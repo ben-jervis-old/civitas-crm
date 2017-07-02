136f17c8a534e34dc34bff019cbaee1412973825
Ben Jervis <ben@jervis.net.au>
2017-07-02 19:49:03 +1000

    Update to User#Index

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,16 +1,20 @@
 class UsersController < ApplicationController
+	def index
+		@users = User.all
+	end
+
 	def new
 		@user = User.new
 	end
-	
+
 	def edit
     @user = User.find(params[:id])
 	end
-	
+
 	def show
     @user = User.find(params[:id])
 	end
-	
+
 	def update
     @user = User.find(params[:id])
     if @user.update_attributes(user_params)
@@ -19,13 +23,13 @@ class UsersController < ApplicationController
       render 'edit'
     end
 	end
-	
+
 	def destroy
     User.find(params[:id]).destroy
     flash[:success] = "User deleted"
     redirect_to users_url
 	end
-	
+
 	private
 
     def user_params
