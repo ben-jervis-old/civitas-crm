35563096d402a981405454995ce864a15cfda60e
Ben Jervis <ben@jervis.net.au>
2017-07-17 18:52:15 +1000

    Fixed page titles

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+	def full_title(page_title = '')
+		base_title = "civitas CRM"
+		if page_title.empty?
+			base_title
+		else
+			page_title + " | " + base_title
+		end
+	end
+
 end
