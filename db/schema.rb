4a44c404fc54b073347a2788d8e356cb637333dc
Ben Jervis <ben@jervis.net.au>
2017-08-10 08:09:43 +1000

    Schema update from new groups changes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170804074501) do
+ActiveRecord::Schema.define(version: 20170808105940) do
 
   create_table "assignments", force: :cascade do |t|
     t.integer  "user_id"
@@ -29,13 +29,15 @@ ActiveRecord::Schema.define(version: 20170804074501) do
     t.integer  "repeat",     default: 0
     t.datetime "created_at",             null: false
     t.datetime "updated_at",             null: false
+    t.string   "event_type"
   end
 
   create_table "groups", force: :cascade do |t|
     t.string   "name"
     t.string   "group_type"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
+    t.string   "description"
   end
 
   create_table "memberships", force: :cascade do |t|
