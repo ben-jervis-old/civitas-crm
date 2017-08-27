099c5d4a13963887b08290916054d23c4fd2e2a8
benJervis <ben@jervis.net.au>
2017-08-27 17:44:14 +1000

    Debounce and Enter added to search

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -21,6 +21,24 @@
 
 var doc_ready;
 doc_ready = function () {
+
+  // Debounce function to limit excessive cycles
+  function debounce(func, wait, immediate) {
+    var timeout;
+    return function() {
+      var context = this, args = arguments;
+      var later = function() {
+        timeout = null;
+        if(!immediate) func.apply(context, args);
+      };
+      var callNow = immediate && !timeout;
+      clearTimeout(timeout);
+      timeout = setTimeout(later, wait);
+      if(callNow) func.apply(context, args);
+    };
+  };
+
+  // Trigger various Bootstrap elements
   $('[data-toggle="tooltip"]').tooltip();
 	$('[data-toggle="popover"]').popover();
 
@@ -67,33 +85,34 @@ doc_ready = function () {
 		}
 	});
 
-	$('#username-search-bar').keyup(function() {
-		var search_string = $(this)[0].value;
-		var filter_string = search_string.toUpperCase();
-		var user_list = $('[data-username]');
+	$('#username-search-bar').keyup(debounce(function(e) {
+    // If enter was pressed, navigate to first result
+    if(e.which == 13) {
+      window.location.href = $('[data-username]:not(.hidden-item)')[0].children[0].children[0].href;
+    }
+    else {
+      var search_string = $(this)[0].value;
+  		var filter_string = search_string.toUpperCase();
+  		var user_list = $('[data-username]');
 
-		for(i = 0; i < user_list.length; i++) {
-			user_name = user_list[i].dataset.username;
-			if(user_name.toUpperCase().indexOf(filter_string) > -1) {
-				user_list[i].classList.remove('hidden-item');
-			}
-			else {
-				user_list[i].classList.add('hidden-item');
-			}
-		}
+  		for(i = 0; i < user_list.length; i++) {
+  			user_name = user_list[i].dataset.username;
+  			if(user_name.toUpperCase().indexOf(filter_string) > -1) {
+  				user_list[i].classList.remove('hidden-item');
+  			}
+  			else {
+  				user_list[i].classList.add('hidden-item');
+  			}
+  		}
 
-		console.log($('[data-username]:not(.hidden-item)'));
-
-		if($('[data-username]:not(.hidden-item)').length == 0) {
-			console.log('Tested');
-			console.log($('#list-empty-msg'));
-			$('#list-empty-msg')[0].classList = 'list-group-item';
-		}
-		else {
-			console.log('Not Tested');
-			$('#list-empty-msg')[0].classList = 'list-group-item hidden-item';
-		}
-	});
+  		if($('[data-username]:not(.hidden-item)').length == 0) {
+  			$('#list-empty-msg')[0].classList = 'list-group-item text-center';
+  		}
+  		else {
+  			$('#list-empty-msg')[0].classList = 'list-group-item hidden-item';
+  		}
+    }
+	}, 100));
 
 	// Flash alerts dismiss after 10 seconds
 	window.setTimeout(function() {
