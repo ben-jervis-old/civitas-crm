149dd5f7c30722030cca8b03f86c03ba0d6e3dff
jrn <jrn889@uowmail.edu.au>
2017-08-30 06:29:39 +0000

    Fixed Notifications pagination

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -6,27 +6,33 @@ class NotificationsController < ApplicationController
   end
   
   def index
-    @users = User.all
-    @user = current_user
-  end
-  
-  def create
+    @staff_users =  User.where(level: %w(staff leader trusted))
+                        .where("id != ?", current_user.id )
+                        .select{ |user| user.notifications.where(read: false).count > 0 }
+    
+    @staff_unread = @staff_users.collect{ |user| { name: user.name, notifs: user.notifications.where(read: false).order(created_at: :desc) } }
+    
+    @unread_notifications = current_user.notifications
+                                        .where(read: false)
+                                        .order(created_at: :desc)
+                                        .paginate(page: params[:page])
+                                        
+    @read_notifications = current_user.notifications
+                                      .where(read: true)
+                                      .order(created_at: :desc)
+                                      .paginate(page: params[:page])
+    
   end
   
   def resolve
     notification = Notification.find(params[:notification_id])
-    notification.read = true
-    notification.save
+    notification.read_by(current_user.id)
 		redirect_to notifications_path
   end
   
   def unresolve
     notification = Notification.find(params[:notification_id])
-    notification.read = false
-    notification.save
+    notification.update_attributes(read: false, read_time: nil, user_id: nil)
 		redirect_to notifications_path
-  end
-  
-  def create
   end
 end
--- app/models/notification.rb
+++ app/models/notification.rb
@@ -1,5 +1,7 @@
 class Notification < ApplicationRecord
   has_and_belongs_to_many :users
+  
+  self.per_page = 10
 
 	def read_by(user_id)
 		self.update_attributes(read: true, read_time: Time.now, user_id: user_id)
