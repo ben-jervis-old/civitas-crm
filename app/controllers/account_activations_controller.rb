5db5189bbdc899c66dd4c5a74370ae9813b0305d
benJervis <ben@jervis.net.au>
2017-09-04 10:59:15 +1000

    Bugfix

--- app/controllers/account_activations_controller.rb
+++ app/controllers/account_activations_controller.rb
@@ -9,7 +9,7 @@ class AccountActivationsController < ApplicationController
       log_in user
       flash[:success] = "Account activated!"
 			notification = Notification.create(title: 'Member Activated', content: "#{user.name} has confirmed their email address.", resolve_link: user_path(user))
-			notification.users << User.where(level: 'staff'))
+			notification.users << User.where(level: 'staff')
       redirect_to root_url
     else
       flash[:danger] = "Invalid activation link"
