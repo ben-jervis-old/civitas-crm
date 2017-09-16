7b1741fc5d664e7f13ed2ad915c67a0181f0350e
benJervis <ben@jervis.net.au>
2017-09-16 16:18:42 +1000

    Cleaned up routes file

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
 
-  # resources :attendances
   resources :events do
-		get 'past', on: :collection
-		get 'users', on: :member
+		get 'past', 			on: :collection
+		get 'users', 			on: :member
 		get 'attendance', on: :member
+		get 'next', 			on: :member
 	end
 
   resources :rosters do
@@ -14,22 +14,22 @@ Rails.application.routes.draw do
 	post '/tasks/:task_id/assign/:user_id', to: 'tasks#assign', as: 'assign_task'
 	delete '/tasks/:task_id/unassign/:user_id', to: 'tasks#unassign', as: 'unassign_task'
 	post '/tasks/:task_id/accept/:user_id', to: 'tasks#accept', as: 'accept_task'
-	post '/notifications/:notification_id/dismiss', to: 'notifications#dismiss', as: 'dismiss_notification'
 
   resources :users do
-  	get 	:privacy, to: 'users#edit_privacy'
+  	get 		:privacy, to: 'users#edit_privacy'
 		patch 	:privacy, to: 'users#update_privacy'
   end
   resources :account_activations, only: [:edit]
   resources :password_resets,     only: [:new, :create, :edit, :update]
   resources :notifications do
-    get    '/resolve', to: 'notifications#resolve', as: 'resolve'
-    get    '/unresolve', to: 'notifications#unresolve', as: 'unresolve'
+		get 	'resolve', 		on: :member
+		get 	'unresolve', 	on: :member
+		post 	'dismiss', 		on: :member
   end
   resources :account_setups,      only: [:edit, :update]
 
   resources :groups do
-    get     '/members', 								to: 'groups#members', 							as: 'members'
+		get     '/members', 								to: 'groups#members', 							as: 'members'
     post    '/assign/:user_id', 				to: 'groups#assign', 								as: 'assign_user'
     delete  '/unassign/:user_id', 			to: 'groups#unassign', 							as: 'unassign_user'
     post    '/make_administrator', 		to: 'groups#make_administrator', 		as: 'make_administrator'
